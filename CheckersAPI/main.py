ca71aa26233b4791de41950428361789984496be
stanislavorlov <stasorlov21@gmail.com>
2025-06-21 22:11:26 +0300

    Adjust per Full-stack structure

--- CheckersAPI/main.py
+++ CheckersAPI/main.py
@@ -1,26 +1,25 @@
 from bson import ObjectId
-from fastapi import APIRouter
+from fastapi import FastAPI
+from database import collection_name
+from schemas import list_games
+from games import Game
 
-from config.database import collection_name
-from models.games import Game
-from schema.schemas import list_games
+app = FastAPI()
 
-router = APIRouter()
-
-@router.get('/')
+@app.get('/')
 async def get_games():
     games = list_games(collection_name.find())
 
     return games
 
-@router.post("/")
+@app.post("/")
 async def post_game(game: Game):
     collection_name.insert_one(dict(game))
 
-@router.put("/{id}")
+@app.put("/{id}")
 async def put_game(id: str, game: Game):
     collection_name.find_and_modify({"_id": ObjectId(id)}, {"$set":dict(game)})
 
-@router.delete("/{id}")
+@app.delete("/{id}")
 async def delete_game(id: str):
     collection_name.find_one_and_delete({"_id": ObjectId(id)})
