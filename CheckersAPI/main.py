3904ea89ebb8937674e004c489df59245cd9347b
stanislavorlov <stasorlov21@gmail.com>
2025-06-24 14:12:20 +0300

    Adding web socket support

--- CheckersAPI/main.py
+++ CheckersAPI/main.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from bson import ObjectId
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-
+from starlette.websockets import WebSocket, WebSocketDisconnect
 from database import collection_name
 from schemas import list_games
 from games import Game
@@ -20,6 +21,24 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str, list[WebSocket]] = defaultdict(list)
+
+    async def connect(self, game_id: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[game_id].append(websocket)
+
+    async def disconnect(self, game_id: str, websocket: WebSocket):
+        self.active_connections[game_id].remove(websocket)
+
+    async def broadcast(self, game_id: str, message: str):
+        if game_id in self.active_connections:
+            for websocket in self.active_connections[game_id]:
+                await websocket.send_text(message)
+
+manager = ConnectionManager()
+
 @app.get('/')
 async def get_games():
     games = list_games(collection_name.find())
@@ -37,3 +56,14 @@ async def put_game(id: str, game: Game):
 @app.delete("/{id}")
 async def delete_game(id: str):
     collection_name.find_one_and_delete({"_id": ObjectId(id)})
+
+@app.websocket("/ws/{game_id}")
+async def websocket_endpoint(websocket: WebSocket, game_id: str):
+    await manager.connect(game_id, websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            await manager.broadcast(game_id, data)
+    except WebSocketDisconnect:
+        await manager.disconnect(game_id, websocket)
