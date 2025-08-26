a1beffb62b93cefba657fbcb72fd5a4eeda4ff02
stanislavorlov <stasorlov21@gmail.com>
2025-08-26 21:35:28 +0300

    bitboard implementation

---
+++ CheckersAPI/domain/bitboard.py
@@ -0,0 +1,163 @@
+class BitboardCheckers:
+    def __init__(self):
+        # bitboards (integers) for different piece types
+        self.black_men   = 0
+        self.black_kings = 0
+        self.white_men   = 0
+        self.white_kings = 0
+        self.MOVE_MAP, self.CAPTURE_MAP = self._generate_maps()
+
+    @staticmethod
+    def bit(square: int) -> int:
+        """Return bit mask for square (1–32)."""
+        return 1 << (square - 1)
+
+    def set_piece(self, square: int, piece: str):
+        """Place a piece on the board."""
+        mask = self.bit(square)
+        if piece == "b":
+            self.black_men |= mask
+        elif piece == "B":
+            self.black_kings |= mask
+        elif piece == "w":
+            self.white_men |= mask
+        elif piece == "W":
+            self.white_kings |= mask
+
+    def remove_piece(self, square: int):
+        """Remove any piece from square."""
+        mask = ~self.bit(square)
+        self.black_men   &= mask
+        self.black_kings &= mask
+        self.white_men   &= mask
+        self.white_kings &= mask
+
+    def piece_at(self, square: int) -> str | None:
+        """Return the piece at a square."""
+        mask = self.bit(square)
+        if self.black_men & mask: return "b"
+        if self.black_kings & mask: return "B"
+        if self.white_men & mask: return "w"
+        if self.white_kings & mask: return "W"
+        return None
+
+    def move_piece(self, from_square: int, to_square: int, piece: str):
+        self.remove_piece(from_square)
+        self.set_piece(to_square, piece)
+
+    def occupancy(self) -> int:
+        """Return bitboard of all occupied squares."""
+        return self.black_men | self.black_kings | self.white_men | self.white_kings
+
+    def print_board(self):
+        """Pretty print board in 8x8 format."""
+        mapping = {None: "."}
+        for sq in range(1, 33):
+            piece = self.piece_at(sq)
+            mapping[sq] = piece if piece else "."
+
+        board = []
+        idx = 1
+        for row in range(8, 0, -1):
+            line = []
+            for col in range(0, 8):
+                if (row + col) % 2 == 0:  # light square
+                    line.append(" ")
+                else:
+                    line.append(mapping[idx])
+                    idx += 1
+            board.append(" ".join(line))
+        print("\n".join(board))
+
+    def generate_moves(self, color: str):
+        """Generate all legal non-capturing moves for given color."""
+        moves = []
+        if color == "black":
+            men, kings = self.black_men, self.black_kings
+            forward_dirs = [(-1, -1), (-1, 1)]  # downwards
+        else:
+            men, kings = self.white_men, self.white_kings
+            forward_dirs = [(1, -1), (1, 1)]  # upwards
+
+        occ = self.occupancy()
+        # Generate moves for men
+        for sq in range(1, 33):
+            if not (men & self.bit(sq)): continue
+            for neigh, _ in self.MOVE_MAP[sq]:
+                if not (occ & self.bit(neigh)):
+                    moves.append((sq, neigh))
+        # Generate moves for kings (both directions)
+        for sq in range(1, 33):
+            if not (kings & self.bit(sq)): continue
+            for neigh, _ in self.MOVE_MAP[sq]:
+                if not (occ & self.bit(neigh)):
+                    moves.append((sq, neigh))
+        return moves
+
+    def generate_captures(self, color: str):
+        """Generate all legal capturing moves for given color."""
+        captures = []
+        if color == "black":
+            men, kings = self.black_men, self.black_kings
+            my_pieces = men | kings
+            opp_pieces = self.white_men | self.white_kings
+        else:
+            men, kings = self.white_men, self.white_kings
+            my_pieces = men | kings
+            opp_pieces = self.black_men | self.black_kings
+
+        occ = self.occupancy()
+        for sq in range(1, 33):
+            if not (my_pieces & self.bit(sq)): continue
+            for neigh, land in self.CAPTURE_MAP[sq]:
+                if (opp_pieces & self.bit(neigh)) and not (occ & self.bit(land)):
+                    captures.append((sq, land, neigh))  # (from, to, jumped)
+        return captures
+
+    def _generate_maps(self):
+        """Generate MOVE_MAP and CAPTURE_MAP automatically for 32 squares."""
+        MOVE_MAP, CAPTURE_MAP = {}, {}
+        board = [[0] * 8 for _ in range(8)]
+        square = 1
+        for row in range(8):
+            for col in range(8):
+                if (row + col) % 2 == 1:  # dark square
+                    board[row][col] = square
+                    square += 1
+
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        for row in range(8):
+            for col in range(8):
+                sq = board[row][col]
+                if sq == 0: continue
+                MOVE_MAP[sq] = []
+                CAPTURE_MAP[sq] = []
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < 8 and 0 <= nc < 8 and board[nr][nc] != 0:
+                        MOVE_MAP[sq].append((board[nr][nc], None))
+                    jr, jc = row + 2 * dr, col + 2 * dc
+                    if (0 <= jr < 8 and 0 <= jc < 8
+                            and board[nr][nc] != 0 and board[jr][jc] != 0):
+                        CAPTURE_MAP[sq].append((board[nr][nc], board[jr][jc]))
+        return MOVE_MAP, CAPTURE_MAP
+
+board = BitboardCheckers()
+
+for sq in range(1, 13):   # white men
+    board.set_piece(sq, "b")
+for sq in range(21, 33):  # black men
+    board.set_piece(sq, "w")
+
+board.move_piece(10, 14, "b")
+board.move_piece(23, 19, "w")
+board.move_piece(11, 15, "b")
+board.move_piece(9, 13, "b")
+board.move_piece(5, 9, "b")
+board.move_piece(1, 5, "b")
+board.move_piece(12, 16, "b")
+
+board.print_board()
+
+print("Moves (white):", board.generate_moves("white"))
+print("Captures (white):", board.generate_captures("white"))
